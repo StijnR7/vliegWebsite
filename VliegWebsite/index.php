c5c30506f9e71047f076feed7ea6ed34792477f5
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2024-06-11 10:46:46 +0200

    zoekreis gefixed

--- VliegWebsite/index.php
+++ VliegWebsite/index.php
@@ -24,11 +24,10 @@
                 <p>CTV vakantie planner is jouw sleutel tot perfecte vakanties</p>
             </div>
             <div class="formvliegut">
-                <form id="rijtje">
-                    <input type="text" placeholder="Vertrekpunt">
-                    <input type="text" placeholder="Bestemming">
-                    <input type="date" placeholder="Vertrek datum">
-                    <input type="date" placeholder="Aankomst">
+                <form id="rijtje" action="zoekreisPHP.php" method="GET" >
+                    <input name="vertrek" type="text" placeholder="Vertrekpunt">
+                    <input name="bestemming" type="text" placeholder="Bestemming">
+                    <input name="verDatum" type="date" placeholder="Vertrek datum">
                     <button type="submit">Zoek</button>
                 </form>
             </div>
