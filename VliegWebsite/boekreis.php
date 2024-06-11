7468c5a71fe2909915ba9c3be5ed54528a77d651
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2024-06-11 11:17:13 +0200

    paar dinge gefixed

--- VliegWebsite/boekreis.php
+++ VliegWebsite/boekreis.php
@@ -1,21 +1,26 @@
 <?php 
     include('connection.php');
     session_start();
-    $vluchtID = $_POST["vluchtID"];
 
-    // Get the userID from the session
+    $vluchtID = $_POST["product_id"];
+    if(isset($_SESSION["userID"])){
     $userID = $_SESSION["userID"];
-    
+   // Prepare the SQL statement with placeholders
+   $sql = "INSERT INTO orderinfo(vluchtID, userID) VALUES (:vluchtID, :userID)";
+   $stmt = $conn->prepare($sql);
 
-        // Prepare the SQL statement with placeholders
-        $sql = "INSERT INTO orderinfo(vluchtID, userID) VALUES (:vluchtID, :userID)";
-        $stmt = $conn->prepare($sql);
-    
-        // Bind the actual values to the placeholders
-        $stmt->bindParam(':vluchtID', $vluchtID);
-        $stmt->bindParam(':userID', $userID);
-    
-        // Execute the statement
-        $stmt->execute();
-    
+   // Bind the actual values to the placeholders
+   $stmt->bindParam(':vluchtID', $vluchtID);
+   $stmt->bindParam(':userID', $userID);
+
+   // Execute the statement
+   $stmt->execute();
+   header('Location: boek.php');
+}
+    else{
+        header('Location: inlog.php');
+
+    }
+
+     
 ?>
