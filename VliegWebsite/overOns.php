c128a3a71a6734936b26b7ed30f70b3c34f323b1
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2024-06-12 12:54:29 +0200

    overOns pagina toegeoevegoend

---
+++ VliegWebsite/overOns.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php include('header.php'); ?>
+    <main>
+        <div class="slideshow-container">
+
+
+            <div class="mySlides fade" style="background-image: url('../vliegWebsite/VliegWebsite/assets/img/Screenshot\ 2024-05-23\ 135248.png');"></div>
+            <div class="mySlides fade" style="background-image: url('../vliegWebsite/VliegWebsite/assets/img/Screenshot\ 2024-05-23\ 135248.png');"></div>
+            <div class="mySlides fade" style="background-image: url('../vliegWebsite/VliegWebsite/assets/img/images\ \(1\).jpeg');"></div> 
+
+
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        </div> 
+      
+        <div class="overlay">
+            <div class="mooitekst">
+                <p>CTV vakantie planner is jouw sleutel tot perfecte vakanties</p>
+            </div>
+            <div class="formvliegut">
+                <form id="rijtje" action="zoekreisPHP.php" method="GET" >
+                    <input name="vertrek" type="text" placeholder="Vertrekpunt">
+                    <input name="bestemming" type="text" placeholder="Bestemming">
+                    <input name="verDatum" type="date" placeholder="Vertrek datum">
+                    <button type="submit">Zoek</button>
+                </for       m>
+            </div>
+         
+            </div>
+    </main> 
+    <script src="assets/javascript/java.js"></script>
+</body>
+</html>
